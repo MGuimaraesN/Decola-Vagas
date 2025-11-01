901bb49ed47e1c93a48010f194b7ceb34610db22
Matheus Guimarães <mguimaraesn.dev@gmail.com>
2025-11-01 18:34:58 -0300

    Create api Login  and Register

---
+++ backend/src/controllers/user.controller.ts
@@ -0,0 +1,125 @@
+import bcrypt from 'bcrypt';
+import type { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import { prisma } from '../database/prisma.js';
+
+export class UserController {
+
+    async register(req: Request, res: Response) {
+        try {
+            const {firstName, lastName, email, password} = req.body;
+        
+            if (!email || !password) {
+                return res.status(400).json(
+                    {error: 'Email e senha são obrigatórios'}
+                )};
+
+            const hashedPassword = await bcrypt.hash(password, 10);
+            const ipUser = req.ip || 'IP não disponível';
+
+            const userExist = await prisma.user.findUnique({
+                where: {email: email}
+            });
+
+            if (userExist) {
+                return res.status(409).json(
+                    {error: 'Email já cadastrado'}
+                )};
+
+            const newUser = await prisma.user.create({
+                data: {
+                    firstName: firstName,
+                    lastName: lastName,
+                    email: email,
+                    password: hashedPassword,
+                    ip: ipUser
+                }});
+
+            const secret = process.env.JWT_SECRET;
+
+            if (!secret) {
+                // Se você esqueceu o .env, dará este erro
+                throw new Error('Secret não está definido!');
+            }
+
+            const payload = {
+                userId: newUser.id,
+                firstName: newUser.firstName,
+                lastName: newUser.lastName,
+                email: newUser.email
+            };
+
+            const token = jwt.sign(payload, secret, { expiresIn: '8h' });
+      
+            return res.status(200).json({
+                access_token: token
+            });
+            
+        } catch (error) {
+            console.error ('Erro ao registrar usuário:', error);
+            return res.status(500).json({error: 'Erro interno do servidor'});
+        };
+    }
+
+    async login(req: Request, res: Response) {
+        try {
+            const {email, password} = req.body;
+            const ipUser = req.ip || 'IP não disponível';
+
+            if (!email || !password) {
+                return res.status(400).json(
+                    {error: 'Email e senha são obrigatórios'}
+                )};
+            
+            const user = await prisma.user.findUnique({
+                where: {email: email}
+            });
+
+            if (!user) {
+                return res.status(401).json(
+                    {error: 'Email ou senha inválidos'}
+                )};
+            
+            const matchedPassword = await bcrypt.compare(password, user.password);
+
+            if (!matchedPassword) {
+                return res.status(401).json(
+                    {error: 'Email ou senha inválidos'}
+                )};
+
+            await prisma.user.update({
+                    where: { id: user.id },
+                    data: {
+                    lastLogin: new Date(),
+                    ip: ipUser
+                    }
+                });
+
+            const secret = process.env.JWT_SECRET;
+
+            if (!secret) {
+                // Se você esqueceu o .env, dará este erro
+                throw new Error('Secret não está definido!');
+            }
+
+            const payload = {
+                userId: user.id,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                email: user.email
+            };
+
+            const token = jwt.sign(payload, secret, { expiresIn: '8h' });
+
+            return res.status(200).json({
+                access_token: token
+            });
+
+        } catch (error) {
+            console.error('Erro ao fazer login:', error);
+            return res.status(500).json({error: 'Erro interno do servidor'});
+            };
+        }
+};
+
+
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,45 +1,8 @@
-// Importações de valor
-import express from 'express';
-import cors from 'cors';
-import { PrismaClient } from '@prisma/client';
+import { app } from './app.js';
 
-// Importações de TIPO (necessário no modo ESM moderno)
-import type { Request, Response } from 'express';
-
-// --- Configuração ---
-const app = express();
-const PORT = process.env.PORT || 5000;
-const prisma = new PrismaClient();
-
-// --- Middlewares ---
-app.use(cors());       // Permite requisições do frontend (localhost:3000)
-app.use(express.json()); // Habilita o parsing de JSON
-
-// --- Rotas da API ---
-app.get('/api/users', async (req: Request, res: Response) => {
-  try {
-    const users = await prisma.user.findMany({
-      orderBy: { createdAt: 'desc' }
-    });
-    res.json(users);
-  } catch (error) {
-    res.status(500).json({ error: 'Erro ao buscar usuários' });
-  }
-});
-
-app.post('/api/users', async (req: Request, res: Response) => {
-  try {
-    const { email, nome } = req.body;
-    const novoUsuario = await prisma.user.create({
-      data: { email, nome },
-    });
-    res.status(201).json(novoUsuario);
-  } catch (error) {
-    res.status(500).json({ error: 'Erro ao criar usuário (email pode já existir)' });
-  }
-});
+const port = process.env.PORT || 5000;
 
 // --- Iniciar Servidor ---
-app.listen(PORT, () => {
-  console.log(`🚀 Backend (ESM) rodando em http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`🚀 Servidor rodando em http://localhost:${port}`);
 });
